bfdfd11f7b9019bf1d89f04a179568fbf819fdc9
Mark Mutugi <mutugimg@gmail.com>
2023-12-14 10:57:06 +0300

    Completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,22 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        
+        json_content = []
+        json_data = json.loads(self.get_response_body())
+        for data in json_data:
+            json_content.append(data)
+
+        return json_content
+    
+custom_url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+response = GetRequester(custom_url)
+
+json_content = response.load_json()
+
+print(json_content)
+
